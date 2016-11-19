8683cc9686ad907c88cb3ad08884b303ec241ede
theMedvedev <medvedev@nyu.edu>
2016-11-19 10:48:28 -0500

    Set MVC structure, added FirebaseUI

---
+++ InBetWin/InBetWin/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  InBetWin
+//
+//  Created by Alexey Medvedev on 11/19/16.
+//  Copyright © 2016 com.AppRising.JAMM. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
