7325bca4fc8a43888a7e55cba028306f1e9eb3fd
jicaey <michaelyoung.dev@gmail.com>
2016-11-20 11:48:17 -0500

    Main View Close

--- InBetWin/InBetWin/UserTableViewCell.swift
+++ InBetWin/InBetWin/UserTableViewCell.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var opponentCardOneImageView: UIImageView!
+    @IBOutlet weak var opponentCardTwoImageView: UIImageView!
+    @IBOutlet weak var cellUsernameLabel: UILabel!
+    @IBOutlet weak var cellBGView: UIView!
+    
+    
+    func updateCellView() {
+                
+        cellUsernameLabel.layer.cornerRadius = 2.0
+        cellBGView.layer.cornerRadius = 2.0
+        
+        
+        cellBGView.layer.masksToBounds = false
+        cellBGView.layer.shadowColor = UIColor.black.cgColor
+        cellBGView.layer.shadowOpacity = 1
+        cellBGView.layer.shadowRadius = CGFloat(5)
+        cellBGView.layer.shadowOffset = CGSize(width: -1, height: 1)
+        
+    }
+}
--- InBetWin/InBetWin/ViewController.swift
+++ InBetWin/InBetWin/ViewController.swift
@@ -8,20 +8,148 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var dealerCardImageView: UIImageView!
+    @IBOutlet weak var playerCardOneImageView: UIImageView!
+    @IBOutlet weak var playerCardTwoImageView: UIImageView!
+    
+    
+    @IBOutlet weak var opponentCardOneImageView: UIImageView!
+    @IBOutlet weak var opponentCardTwoImageView: UIImageView!
+    
+    @IBOutlet weak var dealerCardBGView: UIView!
+    @IBOutlet weak var playerCardOneBGView: UIView!
+    @IBOutlet weak var playerCardTwoBGView: UIView!
+    
+    @IBOutlet weak var cellUsernameLabel: UILabel!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var betAmountLabel: UILabel!
+    @IBOutlet weak var totalTokensLabel: UILabel!
+    
+
+    
+    @IBOutlet weak var higherButton: UIButton!
+    @IBOutlet weak var lowerButton: UIButton!
     
     
     var store = CardAPIClient.shared
     var deck = Deck()
     
+    var betAmountCounter: Int = 0
+    var users: [String] = ["Michael", "Jhantelle", "Alexey", "Arvin", "Juyyt", "kstyi", "zsrhvb", "yxucfiu", "vgcfzchj", "jvfcfzdgg", "hjgdgfzkhbj", "kjgvhhfcxhg"]
+    
+    var flipped = true
+    
+    //test card
+    let front = UIImageView(image: UIImage(named: "cardPlaceholder.png"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //deck.newDeck({ success in print(success) })
         
-        print("ACE".returnValue())
-        print("6".returnValue())
+        updateView()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+    }
+    
+    @IBAction func betUpButtonTouched(_ sender: Any) {
+        betAmountCounter += 5
+        betAmountLabel.text = "$ \(betAmountCounter)"
+        
+    }
+    
+    @IBAction func betDownButtonTouched(_ sender: Any) {
+        betAmountCounter -= 5
+        betAmountLabel.text = "$ \(betAmountCounter)"
+    }
+    
+    @IBAction func betButtonTouched(_ sender: Any) {
+        
+        
 
+        if flipped {
+            UIView.transition(from: playerCardOneImageView, to: front, duration: 0.5, options: UIViewAnimationOptions.transitionFlipFromRight, completion: nil)
+            flipped = false
+        } else {
+            UIView.transition(from: front, to: playerCardOneImageView, duration: 0.5, options: UIViewAnimationOptions.transitionFlipFromLeft, completion: nil)
+            flipped = true
+        }
+
+        
+    }
+    
+    @IBAction func foldButtonTouched(_ sender: Any) {
+    }
+    
+    func updateView() {
+        // Rounded Corners
+        dealerCardImageView.layer.cornerRadius = 5.0
+        dealerCardBGView.layer.cornerRadius = 7.0
+        playerCardOneImageView.layer.cornerRadius = 5.0
+        playerCardOneBGView.layer.cornerRadius = 7.0
+        playerCardTwoImageView.layer.cornerRadius = 5.0
+        playerCardTwoBGView.layer.cornerRadius = 7.0
+        
+        
+        totalTokensLabel.layer.masksToBounds = true
+        totalTokensLabel.layer.cornerRadius = 10.0
+        
+        
+        // Shadow
+        dealerCardBGView.layer.masksToBounds = false
+        dealerCardBGView.layer.shadowColor = UIColor.black.cgColor
+        dealerCardBGView.layer.shadowOpacity = 1
+        dealerCardBGView.layer.shadowRadius = CGFloat(5)
+        
+        playerCardOneBGView.layer.masksToBounds = false
+        playerCardOneBGView.layer.shadowColor = UIColor.black.cgColor
+        playerCardOneBGView.layer.shadowOpacity = 1
+        playerCardOneBGView.layer.shadowRadius = CGFloat(5)
+        
+        playerCardTwoBGView.layer.masksToBounds = false
+        playerCardTwoBGView.layer.shadowColor = UIColor.black.cgColor
+        playerCardTwoBGView.layer.shadowOpacity = 1
+        playerCardTwoBGView.layer.shadowRadius = CGFloat(5)
+        
+        
+    }
+    
+    // TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath) as! UserTableViewCell
+        
+        cell.cellUsernameLabel.text = users[indexPath.row]
+        cell.backgroundColor = UIColor.clear
+        cell.updateCellView()
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.alpha = 0
+        cell.layer.transform = CATransform3DTranslate(CATransform3DIdentity, -250, 0, 0)
+        
+        UIView.animate(withDuration: 0.5) {
+            cell.alpha = 1.0
+            cell.layer.transform = CATransform3DIdentity
+            
+        }
+    }
+    
+    
+    @IBAction func higherButtonTouched(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var lowerButtonTouched: UIButton!
+    
 }
 
